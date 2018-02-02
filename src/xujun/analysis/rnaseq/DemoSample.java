5f6cd93cd7777721c49afa2cf32bf18381b19d26
Jun Xu <Jun Xu@LuLab-PC-02>
2018-02-02 14:29:31 +0800

    trim

--- src/xujun/analysis/rnaseq/DemoSample.java
+++ src/xujun/analysis/rnaseq/DemoSample.java
@@ -29,7 +29,7 @@ public class DemoSample{
 public void test () throws IOException {
         String barcodeFileS = "E:\\experimental data\\RNA_seq\\3'RNAseq barcode.txt";
         String inputDirS ="E:\\experimental data\\RNA_seq\\twice\\clean_data\\";
-        String outputFastaDirS = "E:\\experimental data\\RNA_seq\\twice\\test";        
+        String outputDirS = "E:\\experimental data\\RNA_seq\\twice\\small";        
         RowTable<String> rt = new RowTable<>(barcodeFileS);
         int rowNumber = rt.getRowNumber();
         int columnNumber = rt.getColumnNumber();
@@ -49,29 +49,48 @@ public void test () throws IOException {
         nameSet.parallelStream().forEach(p -> {
             String infile1 = new File (inputDirS, p+"_1.clean.fq").getAbsolutePath();
             String infile2 = new File (inputDirS, p+"_2.clean.fq").getAbsolutePath();
+            String output=new File(outputDirS,"wrong").getAbsolutePath();
             String seq = null;
+            String seq2=null;
             try {
                 BufferedReader br1 = utils.IOUtils.getTextReader(infile1);  
                 BufferedReader br2 = utils.IOUtils.getTextReader(infile2); 
                 BufferedWriter[] bws = new BufferedWriter[rowNumber];
+                BufferedWriter bw=utils.IOUtils.getTextWriter(output);
                 for (int i = 0; i < bws.length; i++) {
-                    String outfileS = new File (outputFastaDirS, nameList.get(i)).getAbsolutePath();
+                    String outfileS = new File (outputDirS, nameList.get(i)).getAbsolutePath();
                     bws[i] = IOUtils.getTextWriter(outfileS);
                 }
-                String temp=null;                                             
-                while ((temp = br1.readLine()) != null){
-                    seq=br1.readLine(); 
+                String temp=null; 
+                String temp2=null;
+                int i=0;
+                while ((temp = br1.readLine()) != null&i<100){
+                    i+=1;
+                    seq=br1.readLine();                    
                     Integer index = barcodeIndexMap.get(seq.substring(0, 8));
                     br1.readLine();br1.readLine();                    
                     if (index == null) {                        
-                        br2.readLine();br2.readLine();br2.readLine();br2.readLine();
+                        bw.write(br2.readLine());bw.write(br2.readLine());bw.write(br2.readLine());bw.write(br2.readLine());
                         continue;
-                    }
-                    bws[index].write(br2.readLine() + "\n");
-                    bws[index].write(br2.readLine() + "\n");
-                    bws[index].write(br2.readLine() + "\n");
-                    bws[index].write(br2.readLine()+"\n");
-                    
+                    }else{
+                        temp2=br2.readLine();
+                        seq2=br2.readLine();
+                        int count=0;
+                        for(int a=0;a<seq2.length();a++){
+                            if(seq2.charAt(a)=='A'){
+                                count++;
+                            }else{
+                                count=0;
+                            }
+                            if(count>7){
+                                seq2=seq2.substring(0, a-7);
+                            }
+                        }                    
+                        bws[index].write(temp2 + "\n");
+                        bws[index].write(seq2 + "\n");
+                        bws[index].write(br2.readLine() + "\n");
+                        bws[index].write(br2.readLine()+"\n");
+                    }                    
                 }
                 br1.close();
                 br2.close();
