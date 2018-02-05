4641106b1a5eb4411b9a919b3628c8f6842135c0
zhaoxueboo <zhaoxuebo16@mails.ucas.ac.cn>
2018-02-05 09:36:08 +0800

    4C_dataAnalysis

---
+++ src/xuebo/analysis/data4C/Entrance4C.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xuebo.analysis.data4C;
+
+/**
+ *
+ * @author xuebozhao
+ */
+//public class Entrance4C {
+//    public static void main (String[] args) { 
+//            String infileS1 = "/Users/xuebozhao/Documents/4Cdata/4C_data_1/test/1.txt";
+//            String infileS2 = "/Users/xuebozhao/Documents/4Cdata/4C_data_1/test/2.txt";
+//            String outfileS1 = "/Users/xuebozhao/Documents/4Cdata/4C_data_1/test/test1111.out.gz";
+//            String outfileS2 = "/Users/xuebozhao/Documents/4Cdata/4C_data_1/test/test2222.out.gz";
+//           
+//            new Containing(infileS1 ,infileS2 , outfileS1,outfileS2);
+//    }
+//}
+
+
+
+public class Entrance4C{
+    
+    public static void main (String[] args) {
+//        
+//        String infileS = "/Users/xuebozhao/Documents/4Cdata/Library/addL23567.sorted.bed";
+//        String outfileS = "/Users/xuebozhao/Documents/4Cdata/Library/addL23567_Filtering.txt";
+////        new FirstStatistics(infileS,outfileS);
+//        new FilteringBed(infileS,outfileS);
+////        new BedUnSorted(infileS,outfileS);
+//      
+
+//        String infileS = "/Users/xuebozhao/Documents/4Cdata/Library/C4_L7.bed";
+//        String outfileS = "/Users/xuebozhao/Documents/4Cdata/Library/C4_L7_BedUnSorted.txt";
+////        new FirstStatistics(infileS,outfileS);
+////        new FilteringBed(infileS,outfileS);
+//        new BedUnSorted(infileS,outfileS);
+
+        String infileS = "/Users/xuebozhao/Documents/4Cdata/Library/QQPlot/positiveNoadjusted_hits_all.bed";
+        String outfileS = "/Users/xuebozhao/Documents/4Cdata/Library/QQPlot/GetCircosBedNoadjusted.bed";
+//        new FirstStatistics(infileS,outfileS);
+        new GetCircosBed (infileS,outfileS);
+        
+        
+//        String outfileS = "/Users/xuebozhao/Documents/4Cdata/Library/GetArabGenomeForCircosChr1.bed";
+////        new FirstStatistics(infileS,outfileS);
+//        new GetArabGenomeForCircos (outfileS);
+        
+    }
+}
+
+//public class Entrance4C{
+//   
+//        public static void main (String[] args) {
+//        
+//            String infileS = "/Users/xuebozhao/Documents/4Cdata/Library/1.txt";
+//            String outfileS = "/Users/xuebozhao/Documents/4Cdata/Library/11111.txt";
+//            new ReducedLibrary(infileS,"TTAA","AGATCT",outfileS);
+//        }
+//        
+//}
---
+++ src/xuebo/analysis/data4C/GetCircosBed.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xuebo.analysis.data4C;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+/**
+ *
+ * @author xuebozhao
+ */
+public class GetCircosBed {
+    
+    GetCircosBed(String infileS,String outfileS) {
+        
+        this.readBed(infileS,outfileS);
+        
+    }
+    
+    public void readBed (String infileS,String outfileS){
+        try{
+            
+            BufferedReader br;            
+            br = IOUtils.getTextReader(infileS);
+            BufferedWriter bw = IOUtils.getTextWriter(outfileS);
+            
+            String temp = null;
+            String L1 = null;
+            String L2 = null;
+            int i = 0;
+
+            while (( temp = br.readLine()) != null) {
+                
+                    ++i;
+                    
+                    if (i % 10 == 0) {
+
+                        System.out.println("Filtering " + i + "....");
+
+                    }
+
+                    String[] tem = temp.split("\t");
+                    
+                    int aa = Integer.valueOf(tem[2]);
+                    
+                    L1 = "chr" + tem[0] + "\t" + tem[1] + "\t" + aa + "\t" + "chr2" + "\t" + "7808877" + "\t" + "7810811";
+                    
+                    bw.write(L1 + "\n"); 
+            }
+            
+            bw.flush(); 
+            bw.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
