cc038be92b110074944d0ef0cebd279e1ed90555
xiaoxiaoerbao <xulaixing2011@163.com>
2019-09-20 22:32:11 +0800

    解决并行错误

--- src/daxing/common/SnpEffSummaryHTML.java
+++ src/daxing/common/SnpEffSummaryHTML.java
@@ -1,16 +1,21 @@
 package daxing.common;
 
 import gnu.trove.list.array.TIntArrayList;
+import org.apache.commons.lang.StringUtils;
 import utils.IOUtils;
-import utils.PStringUtils;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * SnpEff version: SnpEff 4.3t (build 2017-11-24 10:18), by Pablo Cingolani
@@ -109,20 +114,18 @@ public class SnpEffSummaryHTML {
         int num=Integer.MIN_VALUE;
         try {
             String line;
-            String temp=null;
+            List<String> temp=new ArrayList<>();
             br.readLine();
             line=br.readLine();
-            Pattern pattern=Pattern.compile("([1-9]+[,]?)+\\d*");
+            Pattern pattern=Pattern.compile("[1-9][\\d]{0,2}(,\\d{1,3})*");
             Matcher matcher=pattern.matcher(line);
             while (matcher.find()){
-                temp=matcher.group(0);
+                for (int i = 0; i < matcher.groupCount(); i++) {
+                    temp.add(matcher.group(i));
+                }
             }
-            List<String> l= PStringUtils.fastSplit(temp, ",");
-            StringBuilder sb=new StringBuilder();
-            for (int i = 0; i < l.size(); i++) {
-                sb.append(l.get(i));
-            }
-            num=Integer.parseInt(sb.toString());
+            int max=temp.stream().map(str->str.replaceAll(",", "")).mapToInt(Integer::parseInt).max().getAsInt();
+            return max;
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -133,24 +136,22 @@ public class SnpEffSummaryHTML {
         int num=Integer.MIN_VALUE;
         try {
             String line;
-            String temp=null;
+            List<String> temp=new ArrayList<>();
             br.readLine();
             br.readLine();
             br.readLine();
             br.readLine();
             br.readLine();
             line=br.readLine();
-            Pattern pattern=Pattern.compile("([1-9]+[,]?)+\\d*");
+            Pattern pattern=Pattern.compile("[1-9][\\d]{0,2}(,\\d{1,3})*");
             Matcher matcher=pattern.matcher(line);
             while (matcher.find()){
-                temp=matcher.group(0);
+                for (int i = 0; i < matcher.groupCount(); i++) {
+                    temp.add(matcher.group(i));
+                }
             }
-            List<String> l= PStringUtils.fastSplit(temp, ",");
-            StringBuilder sb=new StringBuilder();
-            for (int i = 0; i < l.size(); i++) {
-                sb.append(l.get(i));
-            }
-            num=Integer.parseInt(sb.toString());
+            int max=temp.stream().map(str->str.replaceAll(",", "")).mapToInt(Integer::parseInt).max().getAsInt();
+            return max;
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -211,6 +212,89 @@ public class SnpEffSummaryHTML {
     public double[] getEffectsRegionPercent(){
         double sum= Arrays.stream(numberOfEffectsByRegion).sum();
         return Arrays.stream(this.numberOfEffectsByRegion).mapToDouble(e->e/sum).toArray();
+    }
+
+    public static void getA_B_D_lineageTypeAndRegion(String summaryHtmlFileDir, String outFileDir){
+        File[] input=IOUtils.listRecursiveFiles(new File(summaryHtmlFileDir));
+        Predicate<File> p=File::isHidden;
+        File[] files=Arrays.stream(input).filter(p.negate()).filter(f->f.getName().endsWith("html")).toArray(File[]::new);
+        List<SnpEffSummaryHTML> snpEffSummaryHTMLS=Arrays.stream(files).map(SnpEffSummaryHTML::new).collect(Collectors.toList());
+        int[] a_LineageIndex= IntStream.iterate(1, n->n+6).map(n->n-1).limit(7).toArray();
+        int[] b_LineageIndex=IntStream.iterate(3, n->n+6).map(n->n-1).limit(7).toArray();
+        int[] d_LineageIndex=IntStream.iterate(5, n->n+6).map(n->n-1).limit(7).toArray();
+        int[] typeA, typeB, typeD;
+        int[] typeAll_A=snpEffSummaryHTMLS.get(a_LineageIndex[0]).getNumberOfEffectsByType();
+        int[] typeAll_B=snpEffSummaryHTMLS.get(b_LineageIndex[0]).getNumberOfEffectsByType();
+        int[] typeAll_D=snpEffSummaryHTMLS.get(d_LineageIndex[0]).getNumberOfEffectsByType();
+        double[] percentTypeAll_A, percentTypeAll_B, percentTypeAll_D;
+        for (int i = 1; i < a_LineageIndex.length; i=i+1) {
+            typeA=snpEffSummaryHTMLS.get(a_LineageIndex[i]).getNumberOfEffectsByType();
+            typeAll_A=ArrayTool.add(typeAll_A, typeA);
+        }
+        for (int i = 1; i < b_LineageIndex.length; i=i+1) {
+            typeB=snpEffSummaryHTMLS.get(b_LineageIndex[i]).getNumberOfEffectsByType();
+            typeAll_B=ArrayTool.add(typeAll_B, typeB);
+        }
+        for (int i = 1; i < d_LineageIndex.length; i=i+1) {
+            typeD=snpEffSummaryHTMLS.get(d_LineageIndex[i]).getNumberOfEffectsByType();
+            typeAll_D=ArrayTool.add(typeAll_D, typeD);
+        }
+        int[] regionA, regionB, regionD;
+        int[] regionAll_A=snpEffSummaryHTMLS.get(a_LineageIndex[0]).getNumberOfEffectsByRegion();
+        int[] regionAll_B=snpEffSummaryHTMLS.get(b_LineageIndex[0]).getNumberOfEffectsByRegion();
+        int[] regionAll_D=snpEffSummaryHTMLS.get(d_LineageIndex[0]).getNumberOfEffectsByRegion();
+        double[] percentRegionAll_A, percentRegionAll_B, percentRegionAll_D;
+        for (int i = 1; i < a_LineageIndex.length; i++) {
+            regionA=snpEffSummaryHTMLS.get(a_LineageIndex[i]).getNumberOfEffectsByRegion();
+            regionAll_A=ArrayTool.add(regionAll_A, regionA);
+        }
+        for (int i = 1; i < b_LineageIndex.length; i++) {
+            regionB=snpEffSummaryHTMLS.get(b_LineageIndex[i]).getNumberOfEffectsByRegion();
+            regionAll_B=ArrayTool.add(regionAll_B, regionB);
+        }
+        for (int i = 1; i < d_LineageIndex.length; i++) {
+            regionD=snpEffSummaryHTMLS.get(d_LineageIndex[0]).getNumberOfEffectsByRegion();
+            regionAll_D=ArrayTool.add(regionAll_D, regionD);
+        }
+        percentTypeAll_A=ArrayTool.getElementPercent(typeAll_A);
+        percentTypeAll_B=ArrayTool.getElementPercent(typeAll_B);
+        percentTypeAll_D=ArrayTool.getElementPercent(typeAll_D);
+        percentRegionAll_A=ArrayTool.getElementPercent(regionAll_A);
+        percentRegionAll_B=ArrayTool.getElementPercent(regionAll_B);
+        percentRegionAll_D=ArrayTool.getElementPercent(regionAll_D);
+        String header="A_count\tA_percent\tB_count\tB_percent\tD_count\tD_percent";
+        String columnForType="3_prime_UTR_variant\t5_prime_UTR_premature_start_codon_gain_variant\t5_prime_UTR_variant\tdownstream_gene_variant\tinitiator_codon_variant\tintergenic_region\tintron_variant\tmissense_variant\tsplice_acceptor_variant\tsplice_donor_variant\tsplice_region_variant\tstart_lost\tstop_gained\tstop_lost\tstop_retained_variant\tsynonymous_variant\tupstream_gene_variant";
+        String columnForRegion="DOWNSTREAM\tEXON\tINTERGENIC\tINTRON\tSPLICE_SITE_ACCEPTOR\tSPLICE_SITE_DONOR\tSPLICE_SITE_REGION\tUPSTREAM\tUTR_3_PRIME\tUTR_5_PRIME";
+        String[] typeColumn= StringUtils.split(columnForType, "\t");
+        String[] regionColumn=StringUtils.split(columnForRegion, "\t");
+        try(BufferedWriter bwType=IOUtils.getTextWriter(new File(outFileDir, "type.txt").getAbsolutePath());
+            BufferedWriter bwRegion=IOUtils.getTextWriter(new File(outFileDir, "region.txt").getAbsolutePath())){
+            bwType.write("Type"+"\t"+header);
+            bwRegion.write("Region"+"\t"+header);
+            bwType.newLine();
+            bwRegion.newLine();
+            StringBuilder sb;
+            for (int i = 0; i < typeAll_A.length; i++) {
+                sb=new StringBuilder();
+                sb.append(typeColumn[i]).append("\t").append(typeAll_A[i]).append("\t").append(percentTypeAll_A[i]).append("\t")
+                        .append(typeAll_B[i]).append("\t").append(percentTypeAll_B[i]).append("\t").append(typeAll_D[i]).append("\t")
+                        .append(percentTypeAll_D[i]);
+                bwType.write(sb.toString());
+                bwType.newLine();
+            }
+            bwType.flush();
+            for (int i = 0; i < regionAll_A.length; i++) {
+                sb=new StringBuilder();
+                sb.append(regionColumn[i]).append("\t").append(regionAll_A[i]).append("\t").append(percentRegionAll_A[i]).append("\t")
+                        .append(regionAll_B[i]).append("\t").append(percentRegionAll_B[i]).append("\t").append(regionAll_D[i]).append("\t")
+                        .append(percentRegionAll_D[i]);
+                bwRegion.write(sb.toString());
+                bwRegion.newLine();
+            }
+            bwRegion.flush();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 
