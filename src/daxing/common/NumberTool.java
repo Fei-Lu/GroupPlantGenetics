f55f06103fb17c24af6b851c2c41aafa8e179a81
xiaoxiaoerbao <xulaixing2011@163.com>
2019-10-18 14:28:36 +0800

    千位分隔符

--- src/daxing/common/NumberTool.java
+++ src/daxing/common/NumberTool.java
@@ -1,6 +1,9 @@
 package daxing.common;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.ParseException;
 
 public class NumberTool {
 
@@ -14,4 +17,60 @@ public class NumberTool {
         BigDecimal d=new BigDecimal(num);
         return d.setScale(digit, BigDecimal.ROUND_HALF_UP).doubleValue();
     }
+
+    /**
+     *
+     * @param num  以千位分隔符分隔的字符串数字，如"123,256";
+     * @return
+     */
+    public static int intValue(String num){
+        int res=Integer.MIN_VALUE;
+        try {
+            NumberFormat format=NumberFormat.getInstance();
+            Number number=format.parse(num);
+            res=number.intValue();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+
+    /**
+     *
+     * @param num  以千位分隔符分隔的字符串数字，如"123,453,234.12";
+     * @return
+     */
+    public static double doubleValue(String num){
+        double res=Integer.MIN_VALUE;
+        try {
+            NumberFormat format=NumberFormat.getInstance();
+            Number number=format.parse(num);
+            res=number.doubleValue();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+
+    /**
+     *
+     * @param a
+     * @return
+     */
+    public static String parse(int a){
+        DecimalFormat df = new DecimalFormat();
+        return df.format(a);
+    }
+
+    /**
+     *
+     * @param a
+     * @return
+     */
+    public static String parse(double a){
+        DecimalFormat df = new DecimalFormat();
+        return df.format(a);
+    }
+
+
 }
