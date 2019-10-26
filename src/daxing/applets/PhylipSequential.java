eb1c08d1ca0ae25eb21b31c4e333a4ccc2351655
xiaoxiaoerbao <xulaixing2011@163.com>
2019-10-26 17:23:13 +0800

    modify

--- src/daxing/applets/PhylipSequential.java
+++ src/daxing/applets/PhylipSequential.java
@@ -12,16 +12,17 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.IntStream;
 
 public class PhylipSequential {
 
-    public static void toPhylipSequentialFormat(String vcfInputDir, String chrPosRefTaxonDir, String outFile){
+    public static void toPhylipSequentialFormat(String vcfInputDir, String chrPosRefTaxonDir, String outDir){
         File[] files1=IOUtils.listRecursiveFiles(new File(vcfInputDir));
         File[] files2=IOUtils.listRecursiveFiles(new File(chrPosRefTaxonDir));
         Predicate<File> p=File::isHidden;
         File[] f1= Arrays.stream(files1).filter(p.negate()).toArray(File[]::new);
         File[] f2=Arrays.stream(files2).filter(p.negate()).toArray(File[]::new);
-        StringBuilder sb=new StringBuilder();
+        String[] outNames= Arrays.stream(f1).map(File::getName).map(str->str.substring(0,6)+".txt").toArray(String[]::new);
         if (f1.length!=f2.length){
             System.out.println("please check your "+vcfInputDir+" and "+chrPosRefTaxonDir);
             System.exit(1);
@@ -32,37 +33,19 @@ public class PhylipSequential {
             if (chrF1[i]==chrF2[i]) continue;
             System.out.println("please check your "+vcfInputDir+" and "+chrPosRefTaxonDir);
         }
-        String res=null;
-        for (int i = 0; i < f1.length; i++) {
-            res=PhylipSequential.toPhylipSequentialFormat(f1[i], f2[i]);
-            sb.append(res);
-        }
-        String[] line50=PStringUtils.getMultilineString(50, res);
-        sb=new StringBuilder();
-        sb.append("barley.........").append(line50[0]);
-        try (BufferedWriter bw = IOUtils.getTextWriter(outFile)) {
-            bw.write(sb.toString());
-            bw.newLine();
-            for (int i = 1; i < line50.length; i++) {
-                sb=new StringBuilder();
-                sb.append("...............").append(line50[i]);
-                bw.write(sb.toString());
-                bw.newLine();
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
+        IntStream.range(0, f1.length).parallel().forEach(e->
+                PhylipSequential.toPhylipSequentialFormat(f1[e], f2[e], new File(outDir, outNames[e])));
     }
 
     /**
      *
      * @param vcfInputFile chr
      */
-    private static String toPhylipSequentialFormat(File vcfInputFile, File chrPosRefTaxonFile){
+    private static void toPhylipSequentialFormat(File vcfInputFile, File chrPosRefTaxonFile, File outFile){
         StringBuilder sb=new StringBuilder();
         try (BufferedReader br1 = IOUtils.getTextGzipReader(vcfInputFile.getAbsolutePath());
-             BufferedReader br2 = IOUtils.getTextReader(chrPosRefTaxonFile.getAbsolutePath())) {
+             BufferedReader br2 = IOUtils.getTextReader(chrPosRefTaxonFile.getAbsolutePath());
+             BufferedWriter bw=IOUtils.getTextWriter(outFile.getAbsolutePath())) {
             String line;
             List<String> lineList;
             TIntHashSet chrs=new TIntHashSet();
@@ -81,25 +64,37 @@ public class PhylipSequential {
             int chr=-1;
             int pos=-1;
             int index=-1;
+            int count=0;
+            int total=0;
             while ((line=br2.readLine())!=null){
+                total++;
+                if (sb.length()%50==0 && sb.length()!=0){
+                    sb.append("\n");
+                    bw.write(sb.toString());
+                    sb=new StringBuilder();
+                }
                 lineList=PStringUtils.fastSplit(line);
                 chr=Integer.parseInt(lineList.get(0));
                 pos=Integer.parseInt(lineList.get(1));
                 if (chr!=chrs.iterator().next()){
                     System.out.println("please check your "+vcfInputFile.getName()+" and "+chrPosRefTaxonFile.getName());
+                    System.out.println(chr+"\t"+chrs.iterator().next());
                     System.exit(1);
                 }
                 index=posList.binarySearch(pos);
                 if (index<0){
-                    sb.append("N");
+//                    sb.append("N");
                 }else {
+                    count++;
                     sb.append(lineList.get(3).toUpperCase());
                 }
             }
+            System.out.println("total snp number: "+total+"\t"+"barley having allele snp: "+count);
+            bw.write(sb.toString());
+            bw.newLine();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return sb.toString();
     }
 
 //    public static void main(String[] args) {
