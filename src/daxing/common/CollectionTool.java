e629c8b43e2c75a8639e2d319db83496e0833cac
xiaoxiaoerbao <xulaixing2011@163.com>
2019-11-10 10:12:37 +0800

    int to double

--- src/daxing/common/CollectionTool.java
+++ src/daxing/common/CollectionTool.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class CollectionTool<T> {
 
@@ -31,17 +30,4 @@ public class CollectionTool<T> {
         return list.stream().collect(Collectors.groupingBy(t->t, Collectors.counting()));
     }
 
-    /**
-     *
-     * @param aOrbOrd A B D
-     * @return 1A, 2A, ...7A 或 1B, 2B, ...7B 或 1D, 2D, ...7D
-     */
-    public static List<String> wheatLineageOf(WheatLineage aOrbOrd){
-        List<String> abd=new ArrayList<>();
-        int[] chrA_Lineage= IntStream.range(1, 8).toArray();
-        for (int i = 0; i < chrA_Lineage.length; i++) {
-            abd.add(chrA_Lineage[i]+aOrbOrd.toString());
-        }
-        return abd;
-    }
 }
