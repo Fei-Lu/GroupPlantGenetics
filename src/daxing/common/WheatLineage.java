574e0822599fe319c5ab68bfd87c64aea9698873
xiaoxiaoerbao <xulaixing2011@163.com>
2019-11-20 09:25:51 +0800

    enum WheatLineage

--- src/daxing/common/WheatLineage.java
+++ src/daxing/common/WheatLineage.java
@@ -11,47 +11,51 @@ public enum WheatLineage {
     A, B, D;
 
     /**
-     * 返回A B D对应的ChrID
-     * @param aOrBOrD A B D
-     * @return 1,2, ...
+     *
+     * @return 1,2,7,8, ... or 3,4,9,10, .. or 5,6,11,12, ...
      */
-    public static int[] getWheatLineageOf(WheatLineage aOrBOrD){
-        int[] a= Stream.concat(IntStream.iterate(1, n->n+6).limit(7).boxed(),
-                IntStream.iterate(2, n->n+6).limit(7).boxed()).sorted().mapToInt(Integer::intValue).toArray();
-        int[] b=Stream.concat(IntStream.iterate(3, n->n+6).limit(7).boxed(),
-                IntStream.iterate(4, n->n+6).limit(7).boxed()).sorted().mapToInt(Integer::intValue).toArray();
-        int[] d=Stream.concat(IntStream.iterate(5, n->n+6).limit(7).boxed(),
-                IntStream.iterate(6, n->n+6).limit(7).boxed()).sorted().mapToInt(Integer::intValue).toArray();
-        if (aOrBOrD.toString().equals("A")){
-            return a;
-        }else if (aOrBOrD.toString().equals("B")){
-            return b;
-        }else {
-            return d;
+    public int[] getChrID(){
+        int[] chrID=null;
+        switch (this){
+            case A:
+                chrID=Stream.concat(IntStream.iterate(1, n->n+6).limit(7).boxed(),
+                        IntStream.iterate(2, n->n+6).limit(7).boxed()).sorted().mapToInt(Integer::intValue).toArray();
+                break;
+            case B:
+                chrID=Stream.concat(IntStream.iterate(3, n->n+6).limit(7).boxed(),
+                        IntStream.iterate(4, n->n+6).limit(7).boxed()).sorted().mapToInt(Integer::intValue).toArray();
+                break;
+            case D:
+                chrID=Stream.concat(IntStream.iterate(5, n->n+6).limit(7).boxed(),
+                        IntStream.iterate(6, n->n+6).limit(7).boxed()).sorted().mapToInt(Integer::intValue).toArray();
+                break;
+            default:
+                System.out.println("please input 'A' or 'B' or 'D'");
+
         }
+        return chrID;
     }
 
     /**
      *
-     * @param aOrBOrD A B D
-     * @return 1A, 2A, ...7A 或 1B, 2B, ...7B 或 1D, 2D, ...7D
+     * @return 1A, 2A, ...7A or 1B, 2B, ...7B or 1D, 2D, ...7D
      */
-    public static List<String> wheatLineageOf(WheatLineage aOrBOrD){
-        List<String> abd=new ArrayList<>();
-        int[] chrA_Lineage= IntStream.range(1, 8).toArray();
-        for (int i = 0; i < chrA_Lineage.length; i++) {
-            abd.add(chrA_Lineage[i]+aOrBOrD.toString());
+    public List<String> getChr(){
+        List<String> chrs=new ArrayList<>();
+        int[] chrID1_7= IntStream.range(1, 8).toArray();
+        for (int i = 0; i < chrID1_7.length; i++) {
+            chrs.add(chrID1_7[i]+this.name());
         }
-        return abd;
+        return chrs;
     }
 
     /**
      *
-     * @return 1,2,3,4,7,8, ...
+     * @return 1,2,3,4,7,8,9,10, ...
      */
     public static int[] ablineage(){
-        int[] a=WheatLineage.getWheatLineageOf(WheatLineage.A);
-        int[] b=WheatLineage.getWheatLineageOf(WheatLineage.B);
+        int[] a=WheatLineage.valueOf("A").getChrID();
+        int[] b=WheatLineage.valueOf("B").getChrID();
         return Stream.concat(Arrays.stream(a).boxed(), Arrays.stream(b).boxed()).mapToInt(Integer::intValue).sorted().toArray();
     }
 
@@ -59,8 +63,8 @@ public enum WheatLineage {
      *
      * @return 5,6,11,12, ...
      */
-    public static int[]  dlineage(){
-        return WheatLineage.getWheatLineageOf(WheatLineage.D);
+    public static int[] dlineage(){
+        return WheatLineage.valueOf("D").getChrID();
     }
 
     /**
@@ -68,8 +72,8 @@ public enum WheatLineage {
      * @return 1A, 1B, 2A, 2B, 3A, 3B, ...
      */
     public static List<String> abLineage(){
-        List<String> a=WheatLineage.wheatLineageOf(WheatLineage.A);
-        List<String> b=WheatLineage.wheatLineageOf(WheatLineage.B);
+        List<String> a=WheatLineage.valueOf("A").getChr();
+        List<String> b=WheatLineage.valueOf("B").getChr();
         a.addAll(b);
         return a.stream().sorted().collect(Collectors.toList());
     }
@@ -79,7 +83,7 @@ public enum WheatLineage {
      * @return 1D, 2D, 3D, ...
      */
     public static List<String> dLineage(){
-        return WheatLineage.wheatLineageOf(WheatLineage.D);
+        return WheatLineage.valueOf("D").getChr();
     }
 
     /**
@@ -87,11 +91,9 @@ public enum WheatLineage {
      * @return 1A, 1B, 1D, 2A, 2B, 2D, ...
      */
     public static List<String> abdLineage(){
-        List<String> a=WheatLineage.wheatLineageOf(WheatLineage.A);
-        List<String> b=WheatLineage.wheatLineageOf(WheatLineage.B);
-        List<String> d=WheatLineage.wheatLineageOf(WheatLineage.D);
-        a.addAll(b);
-        a.addAll(d);
-        return a.stream().sorted().collect(Collectors.toList());
+        List<String> ab=WheatLineage.abLineage();
+        List<String> d=WheatLineage.dLineage();
+        ab.addAll(d);
+        return ab.stream().sorted().collect(Collectors.toList());
     }
 }
