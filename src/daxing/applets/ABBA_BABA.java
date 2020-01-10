5ad1c2bf1a81725538a8e9e32cbc5ad08c053d65
xiaoxiaoerbao <xulaixing2011@163.com>
2020-01-10 16:15:13 +0800

    0/0 0/1 1/1 to ref alt allele

--- src/daxing/applets/ABBA_BABA.java
+++ src/daxing/applets/ABBA_BABA.java
@@ -25,7 +25,7 @@ public class ABBA_BABA {
         File[] f1= Arrays.stream(files1).filter(hidden.negate()).toArray(File[]::new);
         File[] f2=Arrays.stream(files2).filter(hidden.negate()).toArray(File[]::new);
         String[] outNames=Arrays.stream(f1).map(File::getName).map(str->str.replaceAll("vcf\\.gz", "geno")).toArray(String[]::new);
-        IntStream.range(0, f1.length).parallel().forEach(e-> convertVCFToGenoFormat(f1[e], f2[e], new File(genoOutDir, outNames[e])));
+        IntStream.range(0, f1.length).forEach(e-> convertVCFToGenoFormat(f1[e], f2[e], new File(genoOutDir, outNames[e])));
     }
 
     private static void convertVCFToGenoFormat(File vcfFile, File ancestralFile, File outFile){
@@ -33,29 +33,31 @@ public class ABBA_BABA {
         try (BufferedReader br1 = IOUtils.getTextGzipReader(vcfFile.getAbsolutePath());
              BufferedWriter bw= IOUtils.getTextWriter(outFile.getAbsolutePath())) {
             String line;
-            List<String> temp;
+            List<String> temp, tem;
             StringBuilder sb=new StringBuilder(100);
             while ((line=br1.readLine()).startsWith("##")){}
             temp=PStringUtils.fastSplit(line);
             temp=temp.stream().skip(9).collect(Collectors.toList());
             sb.append("#CHROM").append("\t").append("POS").append("\t").append(StringUtils.join(temp, "\t"));
+            sb.append("\t").append("Barley");
             bw.write(sb.toString());
             bw.newLine();
             String refAllele, altAllele, outgroupAllele;
             while ((line=br1.readLine())!=null){
                 temp= PStringUtils.fastSplit(line);
+                outgroupAllele=outGroupChrPosAllele.get(temp.get(0), temp.get(1));
                 if (!outGroupChrPosAllele.contains(temp.get(0), temp.get(1))) continue;
+                if (outgroupAllele.equals("-")) continue;
                 refAllele=temp.get(3);
                 altAllele=temp.get(4);
                 sb=new StringBuilder(1000);
                 sb.append(temp.get(0)).append("\t").append(temp.get(1)).append("\t");
-                temp=temp.stream().skip(9).map(str->StringUtils.split(str, ":")[0])
+                tem=temp.stream().skip(9).map(str->StringUtils.split(str, ":")[0])
                         .map(str->str.replaceAll("\\./\\.", "N/N")).collect(Collectors.toList());
-                Collections.replaceAll(temp, "0/0", refAllele+"/"+refAllele);
-                Collections.replaceAll(temp, "0/1", refAllele+"/"+altAllele);
-                Collections.replaceAll(temp, "1/1", altAllele+"/"+altAllele);
-                sb.append(StringUtils.join(temp, "\t"));
-                outgroupAllele=outGroupChrPosAllele.get(temp.get(0), temp.get(1));
+                Collections.replaceAll(tem, "0/0", refAllele+"/"+refAllele);
+                Collections.replaceAll(tem, "0/1", refAllele+"/"+altAllele);
+                Collections.replaceAll(tem, "1/1", altAllele+"/"+altAllele);
+                sb.append(StringUtils.join(tem, "\t"));
                 sb.append("\t").append(outgroupAllele+"/"+outgroupAllele);
                 bw.write(sb.toString());
                 bw.newLine();
