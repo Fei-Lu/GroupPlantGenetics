e41bb88f013f0ed5a801b4ea9c1f22192bf3d6e1
xiaoxiaoerbao <xulaixing2011@163.com>
2020-01-15 14:24:38 +0800

    add log for ABBA_BABA.java

--- src/daxing/applets/ABBA_BABA.java
+++ src/daxing/applets/ABBA_BABA.java
@@ -1,35 +1,40 @@
 package daxing.applets;
 
 import com.google.common.collect.Table;
+import daxing.common.DateTime;
 import daxing.common.RowTableTool;
 import org.apache.commons.lang.StringUtils;
+import utils.Benchmark;
 import utils.IOUtils;
 import utils.PStringUtils;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class ABBA_BABA {
 
-    public static void convertVCFToGenoFormat(String vcfDir, String ancestralVCFDir, String genoOutDir){
-        File[] files1= IOUtils.listRecursiveFiles(new File(vcfDir));
-        File[] files2=IOUtils.listRecursiveFiles(new File(ancestralVCFDir));
-        Predicate<File> hidden=File::isHidden;
-        File[] f1= Arrays.stream(files1).filter(hidden.negate()).toArray(File[]::new);
-        File[] f2=Arrays.stream(files2).filter(hidden.negate()).toArray(File[]::new);
-        String[] outNames=Arrays.stream(f1).map(File::getName).map(str->str.replaceAll("vcf\\.gz", "geno")).toArray(String[]::new);
-        IntStream.range(0, f1.length).forEach(e-> convertVCFToGenoFormat(f1[e], f2[e], new File(genoOutDir, outNames[e])));
+    public static void convertVCFToGenoFormat(String vcfDir, String ancestralVCFDir, String genoOutDir,
+                                              int indexOfAncestralAllele){
+        System.out.println(DateTime.getDateTimeOfNow()+" start");
+        long start=System.nanoTime();
+        List<File> files1= IOUtils.getVisibleFileListInDir(vcfDir);
+        List<File> files2= IOUtils.getVisibleFileListInDir(ancestralVCFDir);
+        String[] outNames=files1.stream().map(File::getName).map(str->str.replaceAll("vcf\\.gz", "geno")).toArray(String[]::new);
+        IntStream.range(0, files1.size()).forEach(e-> convertVCFToGenoFormat(files1.get(e), files2.get(e),
+                new File(genoOutDir, outNames[e]), indexOfAncestralAllele));
+        System.out.println("completed in "+Benchmark.getTimeSpanHours(start)+" hours");
+        System.out.println(DateTime.getDateTimeOfNow()+" end");
     }
 
-    private static void convertVCFToGenoFormat(File vcfFile, File ancestralFile, File outFile){
-        Table<String, String, String> outGroupChrPosAllele= RowTableTool.getTable(ancestralFile.getAbsolutePath(), 5);
+    private static void convertVCFToGenoFormat(File vcfFile, File ancestralFile, File outFile,
+                                               int indexOfAncestralAllele){
+        long start=System.nanoTime();
+        Table<String, String, String> outGroupChrPosAllele= RowTableTool.getTable(ancestralFile.getAbsolutePath(), 2);
         try (BufferedReader br1 = IOUtils.getTextGzipReader(vcfFile.getAbsolutePath());
              BufferedWriter bw= IOUtils.getTextWriter(outFile.getAbsolutePath())) {
             String line;
@@ -39,15 +44,16 @@ public class ABBA_BABA {
             temp=PStringUtils.fastSplit(line);
             temp=temp.stream().skip(9).collect(Collectors.toList());
             sb.append("#CHROM").append("\t").append("POS").append("\t").append(StringUtils.join(temp, "\t"));
-            sb.append("\t").append("Barley");
+            sb.append("\t").append("ancestral");
             bw.write(sb.toString());
             bw.newLine();
             String refAllele, altAllele, outgroupAllele;
             while ((line=br1.readLine())!=null){
                 temp= PStringUtils.fastSplit(line);
-                outgroupAllele=outGroupChrPosAllele.get(temp.get(0), temp.get(1));
                 if (!outGroupChrPosAllele.contains(temp.get(0), temp.get(1))) continue;
+                outgroupAllele=outGroupChrPosAllele.get(temp.get(0), temp.get(1));
                 if (outgroupAllele.equals("-")) continue;
+                if (outgroupAllele.equals("NA")) continue;
                 refAllele=temp.get(3);
                 altAllele=temp.get(4);
                 sb=new StringBuilder(1000);
@@ -67,5 +73,6 @@ public class ABBA_BABA {
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+        System.out.println(outFile.getName()+" completed in "+ Benchmark.getTimeSpanMinutes(start)+" minutes");
     }
 }
