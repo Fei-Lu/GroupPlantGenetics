1918401ba092a47566fcd26906d53efa0ab2c0a6
xiaoxiaoerbao <xulaixing2011@163.com>
2020-04-07 21:19:54 +0800

    load in cultivar and landrace

--- src/daxing/load/GeneAnnotation.java
+++ src/daxing/load/GeneAnnotation.java
@@ -4,7 +4,10 @@ import daxing.applets.PhylipSequential;
 import daxing.common.CollectionTool;
 import daxing.common.IOTool;
 import daxing.common.RowTableTool;
+import gnu.trove.list.array.TDoubleArrayList;
 import gnu.trove.list.array.TIntArrayList;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.math3.util.MathUtils;
 import pgl.infra.table.RowTable;
 import pgl.infra.utils.IOFileFormat;
 import pgl.infra.utils.PStringUtils;
@@ -114,8 +117,7 @@ public class GeneAnnotation {
     public static void countHGDeleteriuos(String geneAnnotationABD, String outFile, int headerIndex){
         // 000,100,010,001,110,101,011,111
         String[] models={"M000","M100","M010","M001","M110","M101","M011","M111"};
-        int[] countHGDeleterious=new int[8];
-        int[] countSyn=new int[8];
+        int[] count=new int[8];
         RowTableTool<String> geneAnnotation=new RowTableTool<>(geneAnnotationABD);
         Predicate<List<String>> pa=l->l.get(1).substring(8,9).equals("A");
         Predicate<List<String>> pb=l->l.get(1).substring(8,9).equals("B");
@@ -123,35 +125,39 @@ public class GeneAnnotation {
         RowTableTool<String> tableA=geneAnnotation.filter(pa);
         RowTableTool<String> tableB=geneAnnotation.filter(pb);
         RowTableTool<String> tableD=geneAnnotation.filter(pd);
+        Comparator<List<String>> c=Comparator.comparing(l->l.get(0));
+        tableA.sortBy(c);
+        tableB.sortBy(c);
+        tableD.sortBy(c);
         for (int i = 0; i < tableA.getRowNumber(); i++) {
             int cntA=Integer.parseInt(tableA.getRow(i).get(headerIndex));
             int cntB=Integer.parseInt(tableB.getRow(i).get(headerIndex));
             int cntD=Integer.parseInt(tableD.getRow(i).get(headerIndex));
             if (cntA==0 && cntB==0 && cntD==0){
-                countHGDeleterious[0]++;
+                count[0]++;
             }else if (cntB==0 && cntD==0){
-                countHGDeleterious[1]++;
+                count[1]++;
             }else if(cntA==0 && cntD==0) {
-                countHGDeleterious[2]++;
+                count[2]++;
             }else if (cntA==0 && cntB==0){
-                countHGDeleterious[3]++;
+                count[3]++;
             }else if (cntD==0){
-                countHGDeleterious[4]++;
+                count[4]++;
             }else if (cntB==0){
-                countHGDeleterious[5]++;
+                count[5]++;
             }else if (cntA==0){
-                countHGDeleterious[6]++;
+                count[6]++;
             }else{
-                countHGDeleterious[7]++;
+                count[7]++;
             }
         }
         try (BufferedWriter bw = IOTool.getTextWriter(outFile)) {
             StringBuilder sb;
             bw.write("model\tcount");
             bw.newLine();
-            for (int i = 0; i < countHGDeleterious.length; i++) {
+            for (int i = 0; i < count.length; i++) {
                 sb=new StringBuilder();
-                sb.append(models[i]).append("\t").append(countHGDeleterious[i]);
+                sb.append(models[i]).append("\t").append(count[i]);
                 bw.write(sb.toString());
                 bw.newLine();
             }
@@ -160,6 +166,82 @@ public class GeneAnnotation {
             e.printStackTrace();
         }
 
+    }
+
+    public static int[] getCount(RowTableTool<String> geneAnnotationABD, int headerIndex){
+        // 000,100,010,001,110,101,011,111
+//        String[] models={"M000","M100","M010","M001","M110","M101","M011","M111"};
+        int[] count=new int[8];
+        Predicate<List<String>> pa=l->l.get(1).substring(8,9).equals("A");
+        Predicate<List<String>> pb=l->l.get(1).substring(8,9).equals("B");
+        Predicate<List<String>> pd=l->l.get(1).substring(8,9).equals("D");
+        RowTableTool<String> tableA=geneAnnotationABD.filter(pa);
+        RowTableTool<String> tableB=geneAnnotationABD.filter(pb);
+        RowTableTool<String> tableD=geneAnnotationABD.filter(pd);
+        Comparator<List<String>> c=Comparator.comparing(l->l.get(0));
+        tableA.sortBy(c);
+        tableB.sortBy(c);
+        tableD.sortBy(c);
+        for (int i = 0; i < tableA.getRowNumber(); i++) {
+            int cntA=Integer.parseInt(tableA.getRow(i).get(headerIndex));
+            int cntB=Integer.parseInt(tableB.getRow(i).get(headerIndex));
+            int cntD=Integer.parseInt(tableD.getRow(i).get(headerIndex));
+            if (cntA==0 && cntB==0 && cntD==0){
+                count[0]++;
+            }else if (cntB==0 && cntD==0){
+                count[1]++;
+            }else if(cntA==0 && cntD==0) {
+                count[2]++;
+            }else if (cntA==0 && cntB==0){
+                count[3]++;
+            }else if (cntD==0){
+                count[4]++;
+            }else if (cntB==0){
+                count[5]++;
+            }else if (cntA==0){
+                count[6]++;
+            }else{
+                count[7]++;
+            }
+        }
+        return count;
+    }
+
+    public static void getCount(String geneAnnotationABD, String outFile){
+        RowTableTool<String> geneAnnotation=new RowTableTool<>(geneAnnotationABD);
+        Set<String> groupset=new HashSet<>(geneAnnotation.getColumn(28));
+        List<String> groups=new ArrayList<>(groupset);
+        RowTableTool<String>[] tables=new RowTableTool[groups.size()];
+        for (int i = 0; i < groups.size(); i++) {
+            int temp=i;
+            tables[i]=geneAnnotation.filter(l->l.get(28).equals(groups.get(temp)));
+        }
+        int[][] delCount=new int[groups.size()][];
+        int[][] nonsynCount=new int[groups.size()][];
+        int[][] synCount=new int[groups.size()][];
+        for (int i = 0; i < groups.size(); i++) {
+            delCount[i]=getCount(tables[i],13);
+            nonsynCount[i]=getCount(tables[i], 8);
+            synCount[i]=getCount(tables[i], 6);
+        }
+        try (BufferedWriter bw = IOTool.getTextWriter(outFile)) {
+            bw.write("model\tdelCount\tnonsynCount\tsynCount\tgroup");
+            bw.newLine();
+            String[] models={"M000","M100","M010","M001","M110","M101","M011","M111"};
+            StringBuilder sb;
+            for (int i = 0; i < groups.size(); i++) {
+                for (int j = 0; j < delCount[i].length; j++) {
+                    sb=new StringBuilder();
+                    sb.append(models[j]).append("\t");
+                    sb.append(delCount[i][j]).append("\t").append(nonsynCount[i][j]).append("\t");
+                    sb.append(synCount[i][j]).append("\t").append(groups.get(i));
+                    bw.write(sb.toString());
+                    bw.newLine();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
