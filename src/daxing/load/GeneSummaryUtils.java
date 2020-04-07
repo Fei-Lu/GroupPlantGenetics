fedc50a3f97f8c83328e7b123d507a428674f546
xiaoxiaoerbao <xulaixing2011@163.com>
2020-04-07 22:09:10 +0800

    GeneSummaryUtils

--- src/daxing/load/GeneSummaryUtils.java
+++ src/daxing/load/GeneSummaryUtils.java
@@ -1,33 +1,16 @@
 package daxing.load;
 
-import daxing.applets.PhylipSequential;
-import daxing.common.CollectionTool;
 import daxing.common.IOTool;
 import daxing.common.RowTableTool;
-import gnu.trove.list.array.TDoubleArrayList;
-import gnu.trove.list.array.TIntArrayList;
-import org.apache.commons.lang3.ArrayUtils;
-import org.apache.commons.math3.util.MathUtils;
 import pgl.infra.table.RowTable;
 import pgl.infra.utils.IOFileFormat;
-import pgl.infra.utils.PStringUtils;
-
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.IOException;
 import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public class GeneAnnotation {
-
-//    public static void retainVmapIIGeneCDS_AndRetainTriad(String geneAnnotationDB, String triadFile){
-//        RowTableTool<String> table=new RowTableTool<>(geneAnnotationDB);
-//        String outFile=geneAnnotationDB.replaceAll("\\.txt\\.gz$","_vmap2.1_cds.txt.gz");
-//        table.write(outFile, IOFileFormat.TextGzip);
-//        retainTriad(triadFile, outFile);
-//    }
+public class GeneSummaryUtils {
 
     public static void retainTriad(String triadFile, String geneAnnotationFile){
         Triad triad=new Triad(triadFile);
@@ -54,7 +37,7 @@ public class GeneAnnotation {
         geneAnnotationTable.write(outFileName, IOFileFormat.TextGzip);
     }
 
-    public static void filterNot6(String geneSummaryTriad, String notTriad, String triadFile){
+    public static void remove169(String geneSummaryTriad, String notTriad, String triadFile){
         Triad triad=new Triad(triadFile);
         RowTable<String> notTriaTable=new RowTable<>(notTriad);
         List<String> notTriadGenes=notTriaTable.getColumn(0);
@@ -66,53 +49,6 @@ public class GeneAnnotation {
         String outFile=geneSummaryTriad.replaceAll("\\.txt\\.gz","_Removed169.txt.gz");
         geneSummaryTable.write(outFile,IOFileFormat.TextGzip);
     }
-//    public static void retainTriad(String triadFile, String geneAnnotationFile){
-//        RowTableTool<String> geneAnnotationDB=new RowTableTool<>(geneAnnotationFile);
-//        Triad triad=new Triad(triadFile);
-//        List<String> subgenomeGenes=triad.getAllGenes();
-//        Predicate<List<String>> inTriad=l->subgenomeGenes.contains(l.get(0).substring(0, 18));
-//        geneAnnotationDB.removeIf(inTriad.negate());
-//        Predicate<List<String>> aSubgenome=l->l.get(0).substring(8,9).equals("A");
-//        Predicate<List<String>> bSubgenome=l->l.get(0).substring(8,9).equals("B");
-//        Predicate<List<String>> dSubgenome=l->l.get(0).substring(8,9).equals("D");
-//        RowTableTool<String> geneAnnotationDB_A=geneAnnotationDB.filter(aSubgenome);
-//        RowTableTool<String> geneAnnotationDB_B=geneAnnotationDB.filter(bSubgenome);
-//        RowTableTool<String> geneAnnotationDB_D=geneAnnotationDB.filter(dSubgenome);
-//        List<String> geneA=geneAnnotationDB_A.getColumn(0);
-//        List<String> geneB=geneAnnotationDB_B.getColumn(0);
-//        List<String> geneD=geneAnnotationDB_D.getColumn(0);
-//        String[] geneNameTriadABD;
-//        List<String> column0;
-//        List<String> geneABD=new ArrayList<>(19000);
-//        Set<String> geneAset=new HashSet<>(geneA);
-//        geneA=new ArrayList<>(geneAset);
-//        for (int i = 0; i < geneA.size(); i++) {
-//            geneNameTriadABD=triad.getTriad(geneA.get(i).substring(0, 18));
-//            column0=geneB.stream().map(s->s.substring(0,18)).collect(Collectors.toList());
-//            int indexB=column0.indexOf(geneNameTriadABD[1]);
-//            if (indexB < 0) continue;
-//            column0=geneD.stream().map(s->s.substring(0,18)).collect(Collectors.toList());
-//            int indexD=column0.indexOf(geneNameTriadABD[2]);
-//            if (indexD < 0) continue;
-//            geneABD.add(geneA.get(i));
-//            geneABD.add(geneB.get(indexB));
-//            geneABD.add(geneD.get(indexD));
-//        }
-//        Predicate<List<String>> p_abd=l->geneABD.contains(l.get(0));
-//        geneAnnotationDB.removeIf(p_abd.negate());
-//        List<String> geneNames=geneAnnotationDB.getColumn(0);
-//        List<String> triadID=new ArrayList<>(19000);
-//        String id;
-//        for (int i = 0; i < geneNames.size(); i++) {
-//            id=triad.getTraidID(geneNames.get(i).substring(0,18));
-//            triadID.add(id);
-//        }
-//        geneAnnotationDB.insertColumn("TriadID",0, triadID);
-//        Comparator<List<String>> c=Comparator.comparing(l->l.get(0));
-//        geneAnnotationDB.sortBy(c);
-//        String outFileName=geneAnnotationFile.replaceAll("\\.txt\\.gz$", "_triad.txt.gz");
-//        geneAnnotationDB.write(outFileName, IOFileFormat.TextGzip);
-//    }
 
     public static void countHGDeleteriuos(String geneAnnotationABD, String outFile, int headerIndex){
         // 000,100,010,001,110,101,011,111
@@ -168,7 +104,7 @@ public class GeneAnnotation {
 
     }
 
-    public static int[] getCount(RowTableTool<String> geneAnnotationABD, int headerIndex){
+    private static int[] getCount(RowTableTool<String> geneAnnotationABD, int headerIndex){
         // 000,100,010,001,110,101,011,111
 //        String[] models={"M000","M100","M010","M001","M110","M101","M011","M111"};
         int[] count=new int[8];
@@ -243,6 +179,12 @@ public class GeneAnnotation {
             e.printStackTrace();
         }
     }
+
+//    public static void main(String[] args) {
+//        retainTriad("","geneSummaryFile");
+//        remove169("", "notTriadFile","");
+//        getCount("triad_removed169File","");
+//    }
 }
 
 
