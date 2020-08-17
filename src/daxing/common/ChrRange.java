2643e34ff3613bd39942a323042038797cc14c85
xiaoxiaoerbao <xulaixing2011@163.com>
2020-08-17 20:04:41 +0800

    introgression per individual

--- src/daxing/common/ChrRange.java
+++ src/daxing/common/ChrRange.java
@@ -1,5 +1,7 @@
 package daxing.common;
 
+import pgl.infra.utils.wheat.RefV1Utils;
+
 /**
  * @author Daxing Xu
  */
@@ -29,5 +31,25 @@ public class ChrRange {
 
     public int getLength(){
         return end-start;
+    }
+
+    public int getChrID(){
+        return RefV1Utils.getChrID(chr, start);
+    }
+
+    public int getVCFStart(){
+        return RefV1Utils.getPosOnChrID(chr, start);
+    }
+
+    public int getVCFEnd(){
+        return RefV1Utils.getPosOnChrID(chr, end);
+    }
+
+    public short getVCFStartChrID(){
+        return (short) RefV1Utils.getChrID(chr, start);
+    }
+
+    public short getVCFEndChrID(){
+        return (short) RefV1Utils.getChrID(chr, end);
     }
 }
