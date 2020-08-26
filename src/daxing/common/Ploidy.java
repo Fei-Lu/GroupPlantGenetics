03c23f14204d3b716a86c90bcf06db6c7fa6fa3e
xiaoxiaoerbao <xulaixing2011@163.com>
2020-08-26 09:07:01 +0800

    Static factory for Ploidy enum in vmapII

--- src/daxing/common/Ploidy.java
+++ src/daxing/common/Ploidy.java
@@ -6,9 +6,9 @@ package daxing.common;
  */
 public enum Ploidy {
 
-    hexaploid("hexaploid", 3, "AABBDD"),
-    tetraploid("tetraploid", 2, "AABB"),
-    diploid("diploid", 1, "DD");
+    hexaploid("hexaploid", 3, "AABBDD","ABD"),
+    tetraploid("tetraploid", 2, "AABB","AB"),
+    diploid("diploid", 1, "DD","D");
 
     public static final Ploidy HEXAPLOID= Ploidy.hexaploid;
     public static final Ploidy TETRAPLOID= Ploidy.tetraploid;
@@ -17,23 +17,29 @@ public enum Ploidy {
     String ploidy;
     int subNum;
     String subChar;
+    String subSingleChr;
 
-    Ploidy(String ploidy, int subgenomowNum, String subChar) {
+    Ploidy(String ploidy, int subgenomowNum, String subChar, String subSingleChar) {
         this.ploidy=ploidy.toLowerCase();
         this.subNum=subgenomowNum;
         this.subChar=subChar;
+        this.subSingleChr = subSingleChar;
     }
 
     public String getPloidy() {
         return ploidy;
     }
 
-    public int getSubgenomowNum() {
+    public int getSubgenomewNum() {
         return subNum;
     }
 
     public String getSubChar() {
         return subChar;
+    }
+
+    public String getSubSingleChr() {
+        return subSingleChr;
     }
 
     public static Ploidy newInstanceFromPloidy(String ploidy){
@@ -50,8 +56,8 @@ public enum Ploidy {
         return null;
     }
 
-    public static Ploidy newInstanceFromSubChar(String subgenomowChar){
-        switch (subgenomowChar.toUpperCase()){
+    public static Ploidy newInstanceFromSubChar(String subgenomeChar){
+        switch (subgenomeChar.toUpperCase()){
             case "AABBDD":
                 return HEXAPLOID;
             case "AABB":
@@ -59,7 +65,21 @@ public enum Ploidy {
             case "DD":
                 return DIPLOID;
             default:
-                System.out.println("please check your parameter: "+subgenomowChar);
+                System.out.println("please check your parameter: "+subgenomeChar);
+        }
+        return null;
+    }
+
+    public static Ploidy newInstanceFromSubSingleChar(String subgenomeSingleChar){
+        switch (subgenomeSingleChar.toUpperCase()){
+            case "ABD":
+                return HEXAPLOID;
+            case "AB":
+                return TETRAPLOID;
+            case "D":
+                return DIPLOID;
+            default:
+                System.out.println("please check your parameter: "+subgenomeSingleChar);
         }
         return null;
     }
