39409ebbd148d0b7745acc9fb86750a478263126
xiaoxiaoerbao <xulaixing2011@163.com>
2020-08-31 11:42:33 +0800

    add heter for IndividualTaxonDerivedProbability

--- src/daxing/load/ancestralSite/complementary/IndividualTaxonDerivedProbability.java
+++ src/daxing/load/ancestralSite/complementary/IndividualTaxonDerivedProbability.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -20,7 +21,7 @@ import java.util.Map;
 public class IndividualTaxonDerivedProbability {
 
     /**
-     *
+     * only TreeValidatedPloidy hexaploid
      * @param inputDir retainTriadHexaploid
      * @param taxa_InfoDBFile
      * @param outFile
@@ -34,13 +35,19 @@ public class IndividualTaxonDerivedProbability {
         Map<String, String> taxonFdBySubContinentMap=RowTableTool.getMap(taxa_InfoDBFile,0, 24);
         try (BufferedWriter bw = IOTool.getWriter(outFile)) {
             bw.write("Taxon\tSub\tSynCount\tNonCount\tDelCount\tDerivedSynCount" +
-                    "\tDerivedNonCount\tDerivedDelCount\tTreeValidatedPloidy\tSubspecies\tfdBySubContinent");
+                    "\tDerivedNonCount\tDerivedDelCount\tHeterSynCount\tHeterNonCount" +
+                    "\tHeterDelCount\tTreeValidatedPloidy" +
+                    "\tSubspecies\tfdBySubContinent");
             bw.newLine();
             String line, taxonName, treeValidatedPloid, subspecies,fdBySubContinent;
-            List<String>[] temp;
+            String subgenome;
+            List<String> temp;
             int numSyn, numNonsyn, numDel, numDerivedSyn, numbDerivedNon, numDerivedDel;
+            int numHeterInSyn, numHeterInNon, numHeterInDel;
+            int subgenomeIndex;
             int[] totalNumDerivedSyn, totalNumDerivedNon, totalNumDerivedDel;
             int[] totalNumSyn, totalNumNon, totalNumDel;
+            int[] totalNumHeterSyn, totalNumHeterNon, totalNumHeterDel;
             Ploidy ploidy;
             StringBuilder sb=new StringBuilder();
             String[] subArray={"A","B","D"};
@@ -49,6 +56,7 @@ public class IndividualTaxonDerivedProbability {
                 br=IOTool.getReader(fileList.get(i));
                 br.readLine();
                 taxonName=PStringUtils.fastSplit(fileList.get(i).getName(), ".").get(0);
+                if (!taxonTreeValidatedPloidyMap.get(taxonName).equals("Hexaploid")) continue;
                 ploidy=Ploidy.newInstanceFromSubChar(taxonPloidyMap.get(taxonName));
                 treeValidatedPloid=taxonTreeValidatedPloidyMap.get(taxonName);
                 subspecies=taxonSubspeciesMap.get(taxonName);
@@ -60,26 +68,31 @@ public class IndividualTaxonDerivedProbability {
                 totalNumDerivedSyn=new int[subNum];
                 totalNumDerivedNon=new int[subNum];
                 totalNumDerivedDel=new int[subNum];
+                totalNumHeterSyn=new int[subNum];
+                totalNumHeterNon=new int[subNum];
+                totalNumHeterDel=new int[subNum];
                 while ((line=br.readLine())!=null){
-                    temp=new List[subNum];
-                    temp[0]=PStringUtils.fastSplit(line);
-                    for (int j = 1; j < temp.length; j++) {
-                        temp[j]=PStringUtils.fastSplit(br.readLine());
-                    }
-                    for (int j = 0; j < temp.length; j++) {
-                        numSyn=Integer.parseInt(temp[j].get(3));
-                        numDerivedSyn=Integer.parseInt(temp[j].get(4));
-                        numNonsyn=Integer.parseInt(temp[j].get(5));
-                        numbDerivedNon=Integer.parseInt(temp[j].get(6));
-                        numDel=Integer.parseInt(temp[j].get(7));
-                        numDerivedDel=Integer.parseInt(temp[j].get(8));
-                        totalNumSyn[j]+=numSyn;
-                        totalNumDerivedSyn[j]+=numDerivedSyn;
-                        totalNumNon[j]+=numNonsyn;
-                        totalNumDerivedNon[j]+=numbDerivedNon;
-                        totalNumDel[j]+=numDel;
-                        totalNumDerivedDel[j]+=numDerivedDel;
-                    }
+                    temp=PStringUtils.fastSplit(line);
+                    subgenome=temp.get(0).substring(8,9);
+                    subgenomeIndex= Arrays.binarySearch(subArray, subgenome);
+                    numSyn=Integer.parseInt(temp.get(1));
+                    numDerivedSyn=Integer.parseInt(temp.get(2));
+                    numHeterInSyn=Integer.parseInt(temp.get(3));
+                    numNonsyn=Integer.parseInt(temp.get(4));
+                    numbDerivedNon=Integer.parseInt(temp.get(5));
+                    numHeterInNon=Integer.parseInt(temp.get(6));
+                    numDel=Integer.parseInt(temp.get(7));
+                    numDerivedDel=Integer.parseInt(temp.get(8));
+                    numHeterInDel=Integer.parseInt(temp.get(9));
+                    totalNumSyn[subgenomeIndex]+=numSyn;
+                    totalNumDerivedSyn[subgenomeIndex]+=numDerivedSyn;
+                    totalNumHeterSyn[subgenomeIndex]+=numHeterInSyn;
+                    totalNumNon[subgenomeIndex]+=numNonsyn;
+                    totalNumDerivedNon[subgenomeIndex]+=numbDerivedNon;
+                    totalNumHeterNon[subgenomeIndex]+=numHeterInNon;
+                    totalNumDel[subgenomeIndex]+=numDel;
+                    totalNumDerivedDel[subgenomeIndex]+=numDerivedDel;
+                    totalNumHeterDel[subgenomeIndex]+=numHeterInDel;
                 }
                 br.close();
                 for (int j = 0; j < subNum; j++) {
@@ -91,6 +104,8 @@ public class IndividualTaxonDerivedProbability {
                     sb.append(totalNumNon[j]).append("\t").append(totalNumDel[j]).append("\t");
                     sb.append(totalNumDerivedSyn[j]).append("\t").append(totalNumDerivedNon[j]).append("\t");
                     sb.append(totalNumDerivedDel[j]).append("\t");
+                    sb.append(totalNumHeterSyn[j]).append("\t").append(totalNumHeterNon[j]).append("\t");
+                    sb.append(totalNumHeterDel[j]).append("\t");
                     sb.append(treeValidatedPloid).append("\t").append(subspecies).append("\t").append(fdBySubContinent);
                     bw.write(sb.toString());
                     bw.newLine();
