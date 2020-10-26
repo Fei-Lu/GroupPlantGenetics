31b0ad65914b8fd0765492e07e88e4c242a9c22f
xiaoxiaoerbao <xulaixing2011@163.com>
2020-10-26 17:01:41 +0800

    by subgenome

--- src/daxing/individualIntrogression/PGI.java
+++ src/daxing/individualIntrogression/PGI.java
@@ -23,23 +23,35 @@ public class PGI {
         Map<String, String> groupBySubspeciesMap= RowTableTool.getMap(taxaInfoDBFile, 23, 25);
         Map<String, String> groupBySubcontinentMap= RowTableTool.getMap(taxaInfoDBFile, 23, 24);
         try (BufferedWriter bw = IOTool.getWriter(outFilePGI)) {
-            bw.write("IntrogressionID\tPGI\tP3\tGroupBySubspecies\tGroupBySubcontinent");
+            bw.write("IntrogressionID\tPGI\tP3\tSub\tGroupBySubspecies\tGroupBySubcontinent");
             bw.newLine();
-            double[] pgi;
+            double[][] pgi;
             String introgressionID;
             String[] p3Array={"WE","DE","FT","AT"};
             Arrays.sort(p3Array);
             StringBuilder sb=new StringBuilder();
+            String sub=null;
             for (int i = 0; i < fileList.size(); i++) {
                 introgressionID=fileList.get(i).getName().substring(0,5);
                 pgi=calculatePGI(fileList.get(i), coordinateFile);
                 for (int j = 0; j < pgi.length; j++) {
-                    sb.setLength(0);
-                    sb.append(introgressionID).append("\t").append(pgi[j]).append("\t");
-                    sb.append(p3Array[j]).append("\t").append(groupBySubspeciesMap.get(introgressionID)).append("\t");
-                    sb.append(groupBySubcontinentMap.get(introgressionID));
-                    bw.write(sb.toString());
-                    bw.newLine();
+                    for (int k = 0; k < pgi[j].length; k++) {
+                        if (pgi[j][k] < 0) continue;
+                        if (j==0 && k==0){
+                            sub="D";
+                        }else if (k==0){
+                            sub="A";
+                        }else if (k==1){
+                            sub="B";
+                        }
+                        sb.setLength(0);
+                        sb.append(introgressionID).append("\t").append(pgi[j][k]).append("\t");
+                        sb.append(p3Array[j]).append("\t").append(sub).append("\t");
+                        sb.append(groupBySubspeciesMap.get(introgressionID)).append("\t");
+                        sb.append(groupBySubcontinentMap.get(introgressionID));
+                        bw.write(sb.toString());
+                        bw.newLine();
+                    }
                 }
             }
             bw.flush();
@@ -48,33 +60,51 @@ public class PGI {
         }
     }
 
-    private static double[] calculatePGI(File fdByIndividualFile, String coordinateFile){
+    private static double[][] calculatePGI(File fdByIndividualFile, String coordinateFile){
         // WE DE FT AT
         String[] p3Array={"WE","DE","FT","AT"};
-        double[] pgiArray=new double[4];
+        double[][] pgiArray=new double[4][2];
         double genomeSize=getGenomeSizeFromCoordinateFile(coordinateFile);
         Arrays.sort(p3Array);
         try (BufferedReader br = IOTool.getReader(fdByIndividualFile)) {
             br.readLine();
-            String line, p3;
+            String line, p3, chrSub;
             List<String> temp;
-            int start, end, p3Index;
+            int start, end, p3Index, p3SubIndex=-1;
             double fd;
             while ((line=br.readLine())!=null){
                 temp=PStringUtils.fastSplit(line, ",");
+                chrSub=temp.get(0).substring(1,2);
                 start=Integer.parseInt(temp.get(1));
                 end=Integer.parseInt(temp.get(2));
                 p3=temp.get(11).substring(0,2);
                 fd=Double.parseDouble(temp.get(9));
                 p3Index=Arrays.binarySearch(p3Array, p3);
-                pgiArray[p3Index]+=fd * (end-start+1);
+                switch (chrSub){
+                    case "A":
+                        p3SubIndex=0;
+                        break;
+                    case "B":
+                        p3SubIndex=1;
+                        break;
+                    case "D":
+                        p3SubIndex=0;
+                        break;
+                }
+                pgiArray[p3Index][p3SubIndex]+=fd * (end-start+1);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        double[] pgi=new double[pgiArray.length];
+        double[][] pgi=new double[pgiArray.length][2];
+        for (int i = 0; i < pgi.length; i++) {
+            Arrays.fill(pgi[i],-1);
+        }
         for (int i = 0; i < pgiArray.length; i++) {
-            pgi[i]=pgiArray[i]/genomeSize;
+            for (int j = 0; j < pgiArray[i].length; j++) {
+                if (i==0 && j ==1) continue;
+                pgi[i][j]=pgiArray[i][j]/genomeSize;
+            }
         }
         return pgi;
     }
