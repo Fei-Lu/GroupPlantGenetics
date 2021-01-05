64e5768e148722d0befc550ba53edfb3ba4abef9
zhangjijin <15600929945@163.com>
2021-01-05 17:18:29 +0800

    Insertion

--- src/jijin/insVerification.java
+++ src/jijin/insVerification.java
@@ -1,13 +1,15 @@
 package jijin;
 
 
-
 import htsjdk.samtools.*;
 import pgl.infra.utils.IOUtils;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class insVerification {
@@ -65,14 +67,76 @@ public class insVerification {
             isIns="0";
         } else {
             for (SAMRecord i : this.reads) {
-                if (i.getCigarString().contains("D")) {
-                    isD.add(1);
+                if (i.getCigar().isClipped()) {//i.getCigarString().contains("D") || i.getCigarString().contains("H")|| i.getCigarString().contains("S")
+//                    if(i.getInferredInsertSize()<0){
+//                        isD.add(0);
+//                    }
+//                    else{
+//                        isD.add(1);
+//                    }
+                    //isD.add(1);
+                    /*
+                    System.out.println(i.getCigar().iterator());
+                    System.out.println(this.breakpoint);
+                    System.out.println(i.getStart()+i.getLengthOnReference());
+                    final Iterator<CigarElement> cigar = i.getCigar().iterator();
+                    */
+                    List<Integer> breakpoint2 = new LinkedList<>();
+                    List<CigarElement> cigar=i.getCigar().getCigarElements();
+                    int bp=i.getAlignmentStart();
+                    breakpoint2.add(bp);
+                    for (CigarElement j : cigar) {
+                        //System.out.println("len"+j.getLength());
+                        //Integer value = Integer.valueOf(j.toString().substring(0, j.toString().length() - 1));
+                        int value = j.getLength();
+                        //System.out.println(j.getOperator());
+                        String state = j.getOperator().toString();//j.toString().substring(j.toString().length() - 1, j.toString().length());
+                        //System.out.println(state);
+                        //breakpoint2.add(i.getAlignmentStart());
+
+                        if (state == "M") {
+                            bp = bp + value;
+                            //System.out.println(bp);
+                            breakpoint2.add(bp);
+
+
+                        } else if (state == "I" || state == "P" || state == "N") {
+                            continue;
+
+                        } else if (state == "D") {
+                            bp = bp + value;
+                            breakpoint2.add(bp);
+
+                        }else{
+                            continue;
+                        }
+
+                    }
+                    List<Integer> posacc = new ArrayList<>();
+                    System.out.println(breakpoint2);
+                    for (int m : breakpoint2){
+                       posacc.add(m-this.breakpoint);
+                    }
+                    //System.out.println(posacc);
+                    //未完
+                    if(posacc.contains(3) || posacc.contains(2) || posacc.contains(1) || posacc.contains(0) || posacc.contains(-1) || posacc.contains(-2) || posacc.contains(-3) ){
+                        isD.add(1);
+                    }else{
+                        isD.add(0);
+                    }
                 } else {
                     isD.add(0);
                 }
+//                    if(i.getInferredInsertSize()>0 && i.getInferredInsertSize()<350){
+//                        isD.add(0);
+//                    }
+//                    else{
 
+                   // }
             }
+
             int occurrences = Collections.frequency(isD,1);
+
             if(occurrences/isD.size()>=0.5){
                 isIns="1";
             }
@@ -91,12 +155,14 @@ public class insVerification {
 
         FileWriter fw = new FileWriter("E:\\Desktop\\三代数据测试\\benchmark\\insertion\\ins.txt");
         BufferedWriter bw = new BufferedWriter(fw);
-        for (int x = 1; x < 1000; x = x + 1) {
+        int line = (int) Files.lines(Paths.get("E:\\Desktop\\三代数据测试\\benchmark\\insertion\\pbmm2-svim_pass_50bp_INS.vcf.bed")).count();
+        System.out.println(line);
+        for (int x = 1; x <= line ; x = x + 1) {
             insVerification iv = new insVerification();
             System.out.println(x);
             iv.getBreakpoint("E:\\Desktop\\三代数据测试\\benchmark\\insertion\\pbmm2-svim_pass_50bp_INS.vcf.bed", x);
-            String a=iv.getReads("E:\\Desktop\\三代数据测试\\benchmark\\insertion\\75.1.bam");
-            System.out.println(a);
+            String a=iv.getReads("E:\\Desktop\\三代数据测试\\benchmark\\insertion\\75.bam");
+            //System.out.println(a);
             bw.write(a);
             bw.newLine();
         }
