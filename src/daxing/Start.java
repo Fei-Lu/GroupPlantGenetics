be60764bfa40b21a13b2bc343aa246c1a2549377
xiaoxiaoerbao <Xudaxing@2018>
2021-09-13 20:54:15 +0800

    remove unused gerp class

--- src/daxing/Start.java
+++ src/daxing/Start.java
@@ -3,7 +3,16 @@ package daxing;
 public class Start {
 
     public static void main(String[] args) {
-        //
+//        String gerpInputDir="/Users/xudaxing/Desktop/test/001_gerp/1A_GERP.bed.sort.gz";
+//        String outDir="/Users/xudaxing/Desktop/test/002_out";
+//        String pgfFile="/Users/xudaxing/Documents/deleteriousMutation/001_analysis/006_vmap2_1062/002_syntenicGene/wheat_v1.1_Lulab.pgf";
+//        String nonoverlapGeneFile="/Users/xudaxing/Documents/deleteriousMutation/001_analysis/006_vmap2_1062/002_syntenicGene/wheat_v1.1_nonoverlap.txt";
+//        GERP.addGERPGeneFuture(gerpInputDir, outDir, pgfFile, nonoverlapGeneFile);
+        System.out.println(2);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 
 }
