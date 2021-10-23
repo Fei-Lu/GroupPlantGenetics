65102c32c82860b59ad9a15ca9d5b4c7ab8935d8
xiaoxiaoerbao <Xudaxing@2018>
2021-10-23 21:51:03 +0800

    P2_hexaplid P3

---
+++ src/daxing/common/factors/HexaploidBySubcontinent.java
@@ -0,0 +1,6 @@
+package daxing.common.factors;
+
+public enum HexaploidBySubcontinent {
+
+    LR_AF, LR_AM, LR_CSA, LR_EA, LR_EU, LR_WA, CL
+}
---
+++ src/daxing/common/factors/P3.java
@@ -0,0 +1,5 @@
+package daxing.common.factors;
+
+public enum P3 {
+    WE,DE,FTT,AT
+}
