24f65d450f3869a83cee15e8c76825125e705130
xiaoxiaoerbao <Xudaxing@2018>
2021-10-24 22:26:47 +0800

    add enum index

--- src/daxing/common/factors/HexaploidBySubcontinent.java
+++ src/daxing/common/factors/HexaploidBySubcontinent.java
@@ -1,6 +1,12 @@
 package daxing.common.factors;
 
+import java.util.Arrays;
+
 public enum HexaploidBySubcontinent {
 
-    LR_AF, LR_AM, LR_CSA, LR_EA, LR_EU, LR_WA, CL
+    LR_AF, LR_AM, LR_CSA, LR_EA, LR_EU, LR_WA, CL;
+
+    public int getIndex(){
+        return Arrays.binarySearch(HexaploidBySubcontinent.values(), this);
+    }
 }
--- src/daxing/common/factors/P3.java
+++
@@ -1,5 +0,0 @@
-package daxing.common.factors;
-
-public enum P3 {
-    WE,DE,FTT,AT
-}
