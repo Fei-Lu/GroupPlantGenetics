5d59c204937679e99fdbb4175a04600c640b5f3f
xiaoxiaoerbao <Xudaxing@2018>
2022-02-14 13:58:57 +0800

    annotation

--- src/daxing/v2/loter/LoterMisc.java
+++ src/daxing/v2/loter/LoterMisc.java
@@ -1,163 +1,83 @@
 package daxing.v2.loter;
 
-import daxing.common.factors.HexaploidBySubcontinent;
-import daxing.common.table.RowTableTool;
-import daxing.common.utiles.ArrayTool;
-import daxing.common.utiles.IOTool;
-import gnu.trove.list.array.TIntArrayList;
 import pgl.infra.utils.Benchmark;
-import pgl.infra.utils.PStringUtils;
+import pgl.infra.utils.IOUtils;
 import pgl.infra.utils.wheat.RefV1Utils;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.IOException;
 import java.text.NumberFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.IntStream;
 
-/**
- * this class contain statics methods calculating individual local ancestry size and individual site percent
- */
 public class LoterMisc {
+
     /**
-     * AB: WE DE FTT
-     * D: ATF ATN
+     *
+     * @param inputFile 有header
+     * @param rate such as 0.01
+     * @param subsetFile 有header
      */
-    private enum AncestryNumber{
-        A(3), B(3), D(2);
-
-        int ancestryNum;
-
-        AncestryNumber(int ancestryNum){
-            this.ancestryNum=ancestryNum;
-        }
-
-        public int getAncestryNum() {
-            return ancestryNum;
-        }
-    }
-
-    public enum GroupInTaxaInfo{
-        Subspecies_by6_TreeValidated(10, "LRCL"), GroupbyContinent(36,"GroupbyContinent"), IfAnalysisByPloidy(11,"Hexaploid");
-
-        int index;
-        String meaning;
-
-        GroupInTaxaInfo(int index, String meaning){
-            this.index=index;
-            this.meaning = meaning;
-        }
-
-        public int getIndex() {
-            return index;
-        }
-
-        public List<String> getGroup(){
-            List<String> groupList = new ArrayList<>();
-            if (this==Subspecies_by6_TreeValidated){
-                groupList.add("Landrace");
-                groupList.add("Cultivar");
-            }else if (this == GroupbyContinent){
-                for (HexaploidBySubcontinent hexaploidBySubcontinent: HexaploidBySubcontinent.values()){
-                    groupList.add(hexaploidBySubcontinent.name());
-                }
-            } else if (this == IfAnalysisByPloidy){
-                groupList.add("Hexaploid");
+    public static void getSubsetFromFile_loterAncestryProportionPerSite(String inputFile, double rate, String subsetFile){
+        long start=System.nanoTime();
+        BufferedReader br;
+        BufferedWriter bw;
+        try {
+            if (inputFile.endsWith("gz")){
+                br= IOUtils.getTextGzipReader(inputFile);
+                bw=IOUtils.getTextGzipWriter(subsetFile);
+            }else {
+                br=IOUtils.getTextReader(inputFile);
+                bw=IOUtils.getTextWriter(subsetFile);
             }
-            return groupList;
-        }
-    }
-
-    public static void calculateIndividualSitePercent(String inputDir, String genotypeDir, String taxaInfo, GroupInTaxaInfo groupInTaxaInfo,
-                                                      String outDir){
-        List<File> laiFiles = IOTool.getFileListInDirEndsWith(inputDir, ".gz");
-        List<File> vcfFiles = IOTool.getFileListInDirEndsWith(genotypeDir, "LRCL.recode.vcf.gz");
-        String[] outNames =
-                laiFiles.stream().map(File::getName).map(s -> s.replaceAll(".txt.gz",".ancestry.txt.gz")).toArray(String[]::new);
-        Map<String, String> taxaGroupMap = RowTableTool.getMap(taxaInfo, 0, groupInTaxaInfo.getIndex());
-        List<String> groupsList = groupInTaxaInfo.getGroup();
-        IntStream.range(0, laiFiles.size()).forEach(e->{
-            long start = System.nanoTime();
-            try (BufferedReader br = IOTool.getReader(laiFiles.get(e));
-                 BufferedReader brGenotype = IOTool.getReader(vcfFiles.get(e));
-                 BufferedWriter bw =IOTool.getWriter(new File(outDir, outNames[e]))) {
-                int chrID =Integer.parseInt(laiFiles.get(e).getName().substring(3,6));
-                String chrSub = RefV1Utils.getChromosome(chrID, 1).substring(1,2);
-                AncestryNumber ancestryNumber= AncestryNumber.valueOf(chrSub);
-                String line;
-                List<String> temp;
-                List<TIntArrayList> individualSNPancestry = new ArrayList<>();
-                List<String> taxaList= new ArrayList<>();
-                TIntArrayList posList = new TIntArrayList();
-                TIntArrayList posAncestry;
-                boolean flag = true;
-                while ((line=br.readLine())!=null){
-                    temp =PStringUtils.fastSplit(line, " ");
-                    posAncestry = new TIntArrayList();
-                    for (String s: temp){
-                        posAncestry.add(Integer.parseInt(s));
-                    }
-                    if (flag){
-                        individualSNPancestry.add(posAncestry);
-                        flag=false;
+            String header=br.readLine();
+            bw.write(header);
+            bw.newLine();
+            String line;
+            double r;
+            int count=0;
+            int total=0;
+            NumberFormat numberFormat=NumberFormat.getInstance();
+            numberFormat.setGroupingUsed(true);
+            int chrID = Integer.parseInt(new File(inputFile).getName().substring(3,6));
+            String subgenome = RefV1Utils.getSubgenomeFromChrID(chrID);
+            while ((line=br.readLine())!=null){
+                total++;
+                r=Math.random();
+                if (r>rate){
+                    if (subgenome.equals("A") || subgenome.equals("B")){
+                        br.readLine();
+                        br.readLine();
+                        continue;
                     }else {
-                        flag =true;
+                        br.readLine();
+                        continue;
                     }
                 }
-                while ((line=brGenotype.readLine()).startsWith("##")){}
-                temp = PStringUtils.fastSplit(line);
-                for (int i = 9; i < temp.size(); i++) {
-                    taxaList.add(temp.get(i));
+                count++;
+                if (subgenome.equals("A") || subgenome.equals("B")){
+                    bw.write(line);
+                    bw.newLine();
+                    line = br.readLine();
+                    bw.write(line);
+                    bw.newLine();
+                    line=br.readLine();
+                    bw.write(line);
+                    bw.newLine();
+                }else {
+                    bw.write(line);
+                    bw.newLine();
+                    line=br.readLine();
+                    bw.write(line);
+                    bw.newLine();
                 }
-                while ((line=brGenotype.readLine())!=null){
-                    temp = PStringUtils.fastSplit(line.substring(0, 30));
-                    posList.add(Integer.parseInt(temp.get(1)));
-                }
-                double[][] ancestryProportion= new double[ancestryNumber.getAncestryNum()][];
-                for (int i = 0; i < ancestryProportion.length; i++) {
-                    ancestryProportion[i] = new double[individualSNPancestry.get(0).size()];
-                    Arrays.fill(ancestryProportion[i], -1);
-                }
-                StringBuilder sb = new StringBuilder();
-                sb.append("Chr\tPos\tGroup\tAncestry\tAncestryProportion");
-                bw.write(sb.toString());
-                bw.newLine();
-                int[] count;
-                NumberFormat numberFormat = NumberFormat.getInstance();
-                numberFormat.setGroupingUsed(false);
-                numberFormat.setMaximumFractionDigits(5);
-                for (int i = 0; i < individualSNPancestry.get(0).size(); i++) {
-                    for (int l = 0; l < groupsList.size(); l++) {
-                        count = new int[ancestryNumber.getAncestryNum()];
-                        for (int j = 0; j < individualSNPancestry.size(); j++) {
-                            if (taxaGroupMap.get(taxaList.get(j)).equals(groupsList.get(l))){
-                                for (int k = 0; k < count.length; k++) {
-                                    if (individualSNPancestry.get(j).get(i) == k){
-                                        count[k]++;
-                                    }
-                                }
-                            }
-                        }
-                        double[] proportion = ArrayTool.getElementPercent(count);
-                        for (int j = 0; j < proportion.length; j++) {
-                            sb.setLength(0);
-                            sb.append(chrID).append("\t").append(posList.get(i)).append("\t");
-                            sb.append(groupsList.get(l)).append("\t");
-                            sb.append(j).append("\t").append(numberFormat.format(proportion[j]));
-                            bw.write(sb.toString());
-                            bw.newLine();
-                        }
-                    }
-                }
-                System.out.println(new File(outDir, outNames[e]).getName() + " completed in "+ Benchmark.getTimeSpanMinutes(start)+
-                        " minutes");
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
             }
-        });
+            br.close();
+            bw.flush();
+            bw.close();
+            System.out.println("samping "+numberFormat.format(count)+"("+numberFormat.format(total)+") row from "
+                    +new File(inputFile).getName()+" into "+new File(subsetFile).getName()+" in "
+                    + Benchmark.getTimeSpanMinutes(start)+" minutes");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
