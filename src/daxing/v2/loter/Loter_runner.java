6ee64978336062c8dd6e8a25d996a62f9eb76cc7
xiaoxiaoerbao <Xudaxing@2018>
2023-01-07 14:08:58 +0800

    loter runner

--- src/daxing/v2/loter/Loter_runner.java
+++ src/daxing/v2/loter/Loter_runner.java
@@ -6,6 +6,7 @@ import com.google.common.collect.Table;
 import daxing.common.sh.CommandUtils;
 import daxing.common.table.RowTableTool;
 import daxing.common.utiles.IOTool;
+import it.unimi.dsi.fastutil.doubles.DoubleArrayList;
 import pgl.infra.utils.PStringUtils;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -101,6 +102,26 @@ public class Loter_runner {
         return allPop;
     }
 
+    public int getSampleSize(String popName){
+        List<String> allPop = this.getAllPop();
+        assert allPop.contains(popName) : "check popName";
+        int count=0;
+        for (Table.Cell<String,String,String> cell : this.taxaInfoMap.cellSet()){
+            if (cell.getValue().equals(popName)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int getAdmixedPopSampleSize(){
+        return this.getSampleSize(this.admixedPop);
+    }
+
+    public int getGenotypeCount(){
+        return this.genotypeID2PathMap.size();
+    }
+
     private void preparePop(){
         Set<String> popSet = RowTableTool.getColumnSet(this.taxaInfoFile, 2);
         Map<String, List<String>> pop2TaxaMap = new HashMap<>();
@@ -148,10 +169,9 @@ public class Loter_runner {
     }
 
     private void run_loter(){
-        StringBuilder sb = new StringBuilder();
-        StringBuilder sbRef = new StringBuilder();
-        int exitCode;
-        for (Map.Entry<String, String> entry:this.genotypeID2PathMap.entrySet()){
+        this.genotypeID2PathMap.entrySet().stream().forEach(entry->{
+            StringBuilder sb = new StringBuilder();
+            StringBuilder sbRef = new StringBuilder();
             sb.setLength(0);
             sb.append("source /Users/xudaxing/anaconda3/bin/activate loter && ");
             sb.append("loter_cli -r ");
@@ -162,14 +182,53 @@ public class Loter_runner {
             }
             sb.append(sbRef).append(" -a ").append(new File(this.subDirFile[1], entry.getKey()+"_"+this.admixedPop+".recode.vcf").getAbsolutePath());
             sb.append(" -f vcf -o ").append(new File(this.subDirFile[2], entry.getKey()+".lai.txt"));
-            exitCode = CommandUtils.runMultipleCommands(sb.toString(), this.outDir, new File(this.logFile));
+            int exitCode = CommandUtils.runMultipleCommands(sb.toString(), this.outDir, new File(this.logFile));
             assert exitCode == 0 : sb+" run failed";
-        }
+        });
     }
 
     public double[][][] extractLocalAncestry(){
+        List<File> laiFiles = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        File laiFile;
+        for (Map.Entry<String, String> entry:this.genotypeID2PathMap.entrySet()){
+            sb.setLength(0);
+            sb.append(entry.getKey()).append(".lai.txt");
+            laiFile = new File(this.subDir[2], sb.toString());
+            laiFiles.add(laiFile);
+        }
+        double[][][] genotype_taxa_variants_localAncestry = new double[this.getGenotypeCount()][][];
+        for (int i = 0; i < laiFiles.size(); i++) {
+            genotype_taxa_variants_localAncestry[i] = this.extractLocalAncestry(laiFiles.get(i));
+        }
+        return genotype_taxa_variants_localAncestry;
+    }
 
-        return null;
+    private double[][] extractLocalAncestry(File laiFile){
+        String line;
+        List<String> temp;
+        DoubleArrayList[] localAncestry = new DoubleArrayList[this.getAdmixedPopSampleSize()];
+        for (int i = 0; i < localAncestry.length; i++) {
+            localAncestry[i] = new DoubleArrayList();
+        }
+        int taxaIndex=0;
+        try (BufferedReader br = IOTool.getReader(laiFile)) {
+            while ((line=br.readLine())!=null){
+                temp =PStringUtils.fastSplit(line, " ");
+                for (int i = 0; i < temp.size(); i++) {
+                    localAncestry[taxaIndex].add(Integer.parseInt(temp.get(i)) == 0 ? 0 : 4);
+                }
+                br.readLine();
+                taxaIndex++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        double[][] localAnc = new double[this.getAdmixedPopSampleSize()][];
+        for (int i = 0; i < localAnc.length; i++) {
+            localAnc[i] = localAncestry[i].toDoubleArray();
+        }
+        return localAnc;
     }
 
 
