3ee3b69b241657856e4efab3d192296fc848b6d2
xiaoxiaoerbao <Xudaxing@2018>
2023-01-10 15:14:02 +0800

    instance of TaxaInfo

---
+++ src/daxing/v2/localAncestryInfer/TaxaInfo.java
@@ -0,0 +1,48 @@
+package daxing.v2.localAncestryInfer;
+
+import com.google.common.collect.Table;
+import daxing.common.table.RowTableTool;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TaxaInfo {
+
+    Table<String,String,String> taxon_Pop_PopName;
+    List<String> popName;
+    Map<String, String> taxaPopNameMap;
+    Map<String, List<String>> popTaxaListMap;
+    public TaxaInfo(String taxaInfo){
+        this.taxon_Pop_PopName = RowTableTool.getTable(taxaInfo, 0 ,1,2);
+        this.popName = new ArrayList<>();
+        this.taxaPopNameMap = new HashMap<>();
+        for (Table.Cell<String,String,String> cell : this.taxon_Pop_PopName.cellSet()){
+            this.taxaPopNameMap.put(cell.getColumnKey(), cell.getValue());
+            this.popName.add(cell.getValue());
+        }
+        this.popTaxaListMap = new HashMap<>();
+        for (String pop : this.popName){
+            this.popTaxaListMap.put(pop, new ArrayList<>());
+        }
+        for (Table.Cell<String,String,String> cell : this.taxon_Pop_PopName.cellSet()){
+            this.popTaxaListMap.get(cell.getValue()).add(cell.getColumnKey());
+        }
+    }
+
+    public List<String> getPopName() {
+        return popName;
+    }
+
+    public Map<String, List<String>> getPopTaxaListMap() {
+        return popTaxaListMap;
+    }
+
+    public Map<String, String> getTaxaPopNameMap() {
+        return taxaPopNameMap;
+    }
+
+    public int getPopSampleSize(String popName){
+        return this.getPopTaxaListMap().get(popName).size();
+    }
+}
