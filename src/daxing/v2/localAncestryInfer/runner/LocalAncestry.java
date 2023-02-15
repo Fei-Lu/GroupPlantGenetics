eed43f9bc0c46ac244e81f84cc7bf2566bc3fe66
xiaoxiaoerbao <Xudaxing@2018>
2023-02-15 22:27:59 +0800

    source order annotation

--- src/daxing/v2/localAncestryInfer/runner/LocalAncestry.java
+++ src/daxing/v2/localAncestryInfer/runner/LocalAncestry.java
@@ -4,8 +4,10 @@ public interface LocalAncestry {
 
     /**
      * @return local ancestry matrix,
-     * dim1 is different run, dim2 is different haplotype
-     * dim3 is source population, dim4 is variants
+     * dim1 is different run,
+     * dim2 is different haplotype
+     * dim3 is source population,  first all introgressed populations, then native population
+     * dim4 is variants
      */
     double[][][][] extractLocalAncestry();
 }
